3fd02669de87436dc7ab169c0936ad82be9d3dc4
sugiy <sugiy.u.2203@gmail.com>
2025-04-29 21:18:56 +0900

    Add Nagoya.swift blog link

--- Sources/Data/BlogPostList.swift
+++ Sources/Data/BlogPostList.swift
@@ -2,6 +2,8 @@ import Foundation
 
 enum BlogPostList {
     static let posts: [BlogPost] = [
+        BlogPost(displayName: "Nagoya.swift＃1やりました #nagoya_swift", link: URL(string: "https://note.com/asashin227/n/nfcfb0b207045")!),
+        BlogPost(displayName: "Nagoya.swiftに行ってきました", link: URL(string: "https://note.com/1mash0/n/n841b3e6cd80e")!),
         BlogPost(displayName: "Minokamo.swift #1 を開催しました！ #minokamo_swift", link: URL(string: "https://note.com/fromkk/n/nc2b4ab7b14e0?sub_rt=share_pb")!),
         BlogPost(displayName: "Minokamo.swift#1 〜foo bar cafeのフィナーレ〜", link: URL(string: "https://www.kamo-it.org/blog/minokamo-swift-vol1/")!),
         BlogPost(displayName: "Osaka.swiftを開催しました", link: URL(string: "https://note.com/totokit4/n/n06f0a62e9832")!),
