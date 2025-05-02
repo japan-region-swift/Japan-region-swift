bc859c6b075049dec3b1164da74ed7552a071f22
treastrain / Tanaka Ryoga <tr@tret.jp>
2025-05-02 23:25:50 +0900

    Package を更新（swift-tools-version: 6.1、swiftLanguageModes: .v6、 twostraws/Ignite の最新版）

--- Package.swift
+++ Package.swift
@@ -1,17 +1,23 @@
-// swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 6.1
 
 import PackageDescription
 
 let package = Package(
-    name: "Japan-Region-Swift",
-    platforms: [.macOS(.v13)],
+    name: "JapanRegionSwiftWebsite",
+    platforms: [.macOS(.v15)],
     dependencies: [
-        .package(url: "https://github.com/twostraws/Ignite.git", branch: "main")
+        .package(
+            url: "https://github.com/twostraws/Ignite",
+            branch: "main"
+        ),
     ],
     targets: [
         .executableTarget(
-            name: "Japan-Region-Swift",
-            dependencies: ["Ignite"]),
-    ]
+            name: "Website",
+            dependencies: [
+                .product(name: "Ignite", package: "Ignite"),
+            ]
+        )
+    ],
+    swiftLanguageModes: [.v6]
 )
