fdd1079e8fcc6094ef597405903a4f89452b6e70
treastrain / Tanaka Ryoga <tr@tret.jp>
2025-05-03 00:29:50 +0900

    `LightTheme` を実装

--- Sources/Themes/LightTheme.swift
+++ Sources/Themes/LightTheme.swift
@@ -10,4 +10,9 @@ import Ignite
 
 struct LightTheme: Theme {
     var colorScheme: ColorScheme = .light
+    var background = Color(hex: "#FFFFFF")
+    var secondaryBackground = Color(hex: "#F7F7F7")
+    var tertiaryBackground = Color(hex: "#E8E8E8")
+    var link = Color.swift
+    var hoveredLink = Color.swift.opacity(0.8)
 }
