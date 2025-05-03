92beabd725672830020b639c4d5a966e495d5756
treastrain / Tanaka Ryoga <tr@tret.jp>
2025-05-03 20:41:18 +0900

    `OriginXPost` を実装

---
+++ Sources/Styles/HideWhenColorSchemeIsLight.swift
@@ -0,0 +1,30 @@
+//
+//  HideWhenColorSchemeIsLight.swift
+//  JapanRegionSwiftWebsite
+//
+//  Created by treastrain on 2025/04/20.
+//
+
+import Foundation
+import Ignite
+
+struct HideWhenColorSchemeIsLight: Style {
+    func style(content: StyledHTML, environment: EnvironmentConditions)
+    -> StyledHTML
+    {
+        switch environment.colorScheme {
+        case .light: content.hidden()
+        case .dark, nil: content
+        }
+    }
+}
+
+extension Style where Self == HideWhenColorSchemeIsLight {
+    static var hiddenWhenLightScheme: Self { Self() }
+}
+
+extension HTML {
+    func hiddenWhenLightScheme() -> some HTML {
+        style(.hiddenWhenLightScheme)
+    }
+}
